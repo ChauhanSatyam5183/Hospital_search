3f23d62daf2eaaf675cd5e8286623d11d4ac62ca
satyam chauhan <138180169+ChauhanSatyam5183@users.noreply.github.com>
2025-02-16 01:00:54 +0530

    f add

---
+++ src/Components/Banner2.jsx
@@ -0,0 +1,21 @@
+import Card from "./Card"
+import list from "../../public/list.json"
+function Banner2() {
+   console.log("list adta",list);
+
+          
+
+  return (
+    <>    <h1 className="font-bold text-xl pb-2 text-center " >Our Services</h1>
+         <div className="grid grid-cols-1 md:grid-cols-3  justify-evenly space-x-9">
+       { list.map((item)=>(
+                    <Card item={item} key={item}/>
+                )
+                )}
+     </div>
+    </>
+    
+  )
+}
+
+export default Banner2
---
+++ src/Courses/Courses.jsx
@@ -0,0 +1,19 @@
+
+import Navbar from '../Components/Navbar'
+import Course from '../Components/Course'
+import Footer from '../Components/Footer'
+
+function Courses() {
+   
+  return (
+    <>
+      <Navbar/>
+      <div className='min-h-screen'>
+        <Course/>
+      </div>
+      <Footer/>
+    </>
+  )
+}
+
+export default Courses
